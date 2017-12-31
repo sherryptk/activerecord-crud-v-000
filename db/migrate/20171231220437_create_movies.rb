9c81dbb7b40ff7a5b361322df93e3bf484c339dd
Sherry El Garhy <sherryptk@gmail.com>
2017-12-31 22:06:29 +0000

    automatically backed up by learn

--- db/migrate/20171231220437_create_movies.rb
+++ db/migrate/20171231220437_create_movies.rb
@@ -1,4 +1,15 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.date :release_date
+      t.string :director
+      t.integer :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+
   end
 end
